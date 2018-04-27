069451757b70c81d269e5f5f7a7b70464712d0b9
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-04-27 11:28:59 +0900

    [add] linker shellscript and .ssh/config

---
+++ dotfilesLink.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+# set -x
+
+ln -nfs ~/dotfiles/.atom/config.cson  ~/.atom/config.cson
+ln -nfs ~/dotfiles/.atom/init.coffee  ~/.atom/init.coffee
+ln -nfs ~/dotfiles/.atom/keymap.cson  ~/.atom/keymap.cson
+ln -nfs ~/dotfiles/.atom/snippets.cson ~/.atom/snippets.cson
+ln -nfs ~/dotfiles/.atom/styles.less  ~/.atom/styles.less
+ln -nfs ~/dotfiles/.bash_profile  ~/.bash_profile
+ln -nfs ~/dotfiles/.ssh/config ~/.ssh/config
